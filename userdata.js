e4daca90cb3b053275f65096fb38531ca615176a
Thomas Durlacher <thomas.durlacher@gmail.com>
2022-06-17 08:22:16 +0200

    scoreboard

--- userdata.js
+++ userdata.js
@@ -4,28 +4,36 @@ const users = [
       id: 1,
       name: "thomas",
       email: "thomas@gmail.com",
-      password: "test"
+      password: "test",
+      score: 3,
+      question: 12
     },
   
     {
       id: 2,
       name: "lisa",
       email: "lisa@gmail.com",
-      password: "test"
+      password: "test",
+      score: 3,
+      question: 12
     },
   
     {
       id: 3,
       name: "enes",
       email: "enes@gmail.com",
-      password: "test"
+      password: "test",
+      score: 3,
+      question: 12
     },
   
     {
       id: 4,
       name: "jon",
       email: "jon@gmail.com",
-      password: "test"
+      password: "test",
+      score: 3,
+      question: 12
     }
   
   ];
