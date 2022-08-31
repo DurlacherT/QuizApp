a4f4a30c4a14496f4dba7d25af5810f40b41c8e4
Thomas Durlacher <thomas.durlacher@gmail.com>
2022-08-31 19:27:18 +0200

    beautify

--- frontend/login.js
+++ frontend/login.js
@@ -2,100 +2,106 @@
  * login.js declares functions which make DELETE, PUT, POST requests (GET is used in mainquiz.js)
  * The functions are called in login.html
  */
-
-var currentUser = 'Anonymous'
-// Function to make a HTTP DELETE request
-function deleteUser() {
-    const deleteUser = {
-        name: document.getElementById("username").value,
-      };
-        const response = fetch('http://localhost:8000/api/users/:name', {
-            method: 'DELETE',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(deleteUser),
-        });
-        console.log("1 record deleted");
-        alert( `User ${deleteUser.name} deleted.`);
-
-    }
-
-    // Function to make a HTTP PUT request
-    function updateUser() {
-        const updateUser = {
-            name: document.getElementById("username").value,
-            email: document.getElementById("email").value,
-            password: document.getElementById("password").value,
-          };
-        const response = fetch('http://localhost:8000/api/users/:name', {
-            method: 'PUT',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(updateUser),
-        });
-        console.log("record updated");
-        alert( `User ${updateUser.name} updated.`);
-    }
-
-    // Make a HTTP POST request
-    function loginUser() {
-        const newUser = {
-            name: document.getElementById("username").value,
-            email: document.getElementById("email").value,
-            password: document.getElementById("password").value,
-          };
-
-          currentUser = document.getElementById("username").value;
-        const response = fetch('http://localhost:8000/api/users/authenticate', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(newUser),
-        
-}); 
-
-    }
-
-    // Make a HTTP POST request
-    function createUser() {
-        const newUser = {
-            name: document.getElementById("username").value,
-            email: document.getElementById("email").value,
-            password: document.getElementById("password").value,
-          };
-        fetch('http://localhost:8000/api/users/', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(newUser),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-        }); 
-        alert( `New user ${newUser.name} created.`);
-    }
-
-    // Make a HTTP POST request
-    function logoutUser() {
-        const updateUser = {
-            name: document.getElementById("username").value,
-            email: document.getElementById("email").value,
-            password: document.getElementById("password").value,
-          };
-        fetch('http://localhost:8000/api/users/logout', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(updateUser),
-        })
-        alert( `User logged out.`);
-    }
+ let currentUser = 'Anonymous'
+ // Function to make a HTTP DELETE request
+ function deleteUser() {
+     const deleteUser = {
+         name: document.getElementById("username")
+             .value
+     , };
+     const response = fetch('http://localhost:8000/api/users/:name', {
+         method: 'DELETE'
+         , headers: {
+             'Content-type': 'application/json'
+         }
+         , body: JSON.stringify(deleteUser)
+     , });
+     console.log("1 record deleted");
+     alert(`User ${deleteUser.name} deleted.`);
+ }
+ // Function to make a HTTP PUT request
+ function updateUser() {
+     const updateUser = {
+         name: document.getElementById("username")
+             .value
+         , email: document.getElementById("email")
+             .value
+         , password: document.getElementById("password")
+             .value
+     , };
+     const response = fetch('http://localhost:8000/api/users/:name', {
+         method: 'PUT'
+         , headers: {
+             'Content-type': 'application/json'
+         }
+         , body: JSON.stringify(updateUser)
+     , });
+     console.log("record updated");
+     alert(`User ${updateUser.name} updated.`);
+ }
+ // Make a HTTP POST request
+ function loginUser() {
+     const newUser = {
+         name: document.getElementById("username")
+             .value
+         , email: document.getElementById("email")
+             .value
+         , password: document.getElementById("password")
+             .value
+     , };
+     currentUser = document.getElementById("username")
+         .value;
+     const response = fetch('http://localhost:8000/api/users/authenticate', {
+         method: 'POST'
+         , headers: {
+             'Content-type': 'application/json'
+         }
+         , body: JSON.stringify(newUser)
+     , });
+ }
+ // Make a HTTP POST request
+ function createUser() {
+     const newUser = {
+         name: document.getElementById("username")
+             .value
+         , email: document.getElementById("email")
+             .value
+         , password: document.getElementById("password")
+             .value
+     , };
+     fetch('http://localhost:8000/api/users/', {
+             method: 'POST'
+             , headers: {
+                 'Content-type': 'application/json'
+             }
+             , body: JSON.stringify(newUser)
+         , })
+         .then((response) => response.json())
+         .then((data) => {
+             console.log('Success:', data);
+         })
+         .catch((error) => {
+             console.error('Error:', error);
+         });
+     alert(`New user ${newUser.name} created.`);
+ }
+ // Make a HTTP POST request
+ function logoutUser() {
+     const updateUser = {
+         name: document.getElementById("username")
+             .value
+         , email: document.getElementById("email")
+             .value
+         , password: document.getElementById("password")
+             .value
+     , };
+     fetch('http://localhost:8000/api/users/logout', {
+         method: 'POST'
+         , headers: {
+             'Content-type': 'application/json'
+         }
+         , body: JSON.stringify(updateUser)
+     , })
+     alert(`User logged out.`);
+ }
+ 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,128 +1,119 @@
 /**
  * users.js consists of routes that provide data of the server to the client/web browser
- * The routes are exported to index.js. We use get, put, post and delete route methods.
+ * The routes are exported to server.js. We use get, put, post and delete route methods.
  * users.js also requires the express-session module which is used for session management
  */
-
-
 //use express-session for session management
 const express = require("express");
 const router = express.Router();
 let con = require("../../mysqlconnection");
-
 const sessions = require('express-session');
 router.use(sessions({
-  secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-  saveUninitialized: true,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 },
-  resave: false
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767"
+    , saveUninitialized: true
+    , cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+    , resave: false
 }));
-var session;
-
+let session;
 //Get user data --->
 router.get("/", (req, res) => {
-  var sql = "SELECT * FROM accounts";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    res.json(result);})
+    let sql = "SELECT * FROM accounts";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    })
 });
-
 //Create new user --->
 router.post("/", (req, res) => {
-  var sql = "INSERT INTO accounts (username, password, email, question, score) VALUES ('" + req.body.name + "', '" + req.body.password + "', '" + req.body.email + "', '" + 0 + "' , '" + 0 + "')";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
+    let sql = "INSERT INTO accounts (username, password, email, question, score) VALUES ('" + req.body
+        .name + "', '" + req.body.password + "', '" + req.body.email + "', '" + 0 + "' , '" + 0 +
+        "')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
 });
-
 //Update User --->
 router.put("/:name", (req, res) => {
-  var sql = "UPDATE accounts SET email = '" + req.body.email + "', password = '" + req.body.password + "' WHERE username ='" + req.body.name + "'";
-  con.query(sql, function (err, result) {
-  })
+    let sql = "UPDATE accounts SET email = '" + req.body.email + "', password = '" + req.body
+        .password + "' WHERE username ='" + req.body.name + "'";
+    con.query(sql, function (err, result) {})
 });
-
 //Update User --->
 router.put('/', (req, res) => {
-  console.log("test");
-
-  console.log(req.body.question);
-
-  var sql = "UPDATE accounts SET score = '" + req.body.score + "', question = '" + req.body.question + "' WHERE username ='" + req.body.name + "'";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    res.json(result);
-  })
+    console.log("test");
+    console.log(req.body.question);
+    let sql = "UPDATE accounts SET score = '" + req.body.score + "', question = '" + req.body
+        .question + "' WHERE username ='" + req.body.name + "'";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    })
 });
-
 //Delete User --->
 router.delete("/:name", (req, res) => {
-  console.log(req.body.name);
-
-  var sql = "DELETE FROM accounts WHERE username ='" + req.body.name + "'";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-  })
- })
-
+    console.log(req.body.name);
+    let sql = "DELETE FROM accounts WHERE username ='" + req.body.name + "'";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+    })
+})
 //Authenticate user --->
-router.post('/authenticate',(req,res) => {
-  var sql = "SELECT * FROM accounts";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    let mypassword;
-    mypassword = false;
-    for (let i = 0; i < result.length; i++) {
-      if(result[i].email === req.body.email && result[i].password === req.body.password)
-       {mypassword = true;}
-      }
-    if(mypassword){
-      session=req.session;
-      session.userid=req.body.name;
-
-        console.log(session);
-        console.log("Logged in");
-
-        var sql = "UPDATE accounts set sessionkey = '" + req.session.id + "' WHERE username ='" + req.body.name + "'";
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-        });
-        //res.sendFile('frontend/index.html',{root:__dirname});
-    }
-    else{
-        console.log("Wrong username or password!");
-    }
-  });
- })
- 
- //Logout user --->
-router.post('/logout',(req,res) => {
-  var sql = "UPDATE accounts set sessionkey = NULL WHERE username ='" + req.body.name + "'";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-  });
-  req.session.destroy((err) => {
-      if(err) {
-          return console.log(err);
-      }
-      res.redirect('/');
-  });
-  
-  console.log("req.session");
-  console.log(req.session);
-  console.log(session);
+router.post('/authenticate', (req, res) => {
+    var sql = "SELECT * FROM accounts";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        let mypassword;
+        mypassword = false;
+        for (let i = 0; i < result.length; i++) {
+            if (result[i].email === req.body.email && result[i].password === req.body
+                .password) {
+                mypassword = true;
+            }
+        }
+        if (mypassword) {
+            session = req.session;
+            session.userid = req.body.name;
+            console.log(session);
+            console.log("Logged in");
+            var sql = "UPDATE accounts set sessionkey = '" + req.session.id +
+                "' WHERE username ='" + req.body.name + "'";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+            });
+        } else {
+            console.log("Wrong username or password!");
+        }
+    });
+})
+//Logout user --->
+router.post('/logout', (req, res) => {
+    let sql = "UPDATE accounts set sessionkey = NULL WHERE username ='" + req.body.name + "'";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+    });
+    req.session.destroy((err) => {
+        if (err) {
+            return console.log(err);
+        }
+        res.redirect('/');
+    });
+    console.log("req.session");
+    console.log(req.session);
+    console.log(session);
 });
-
-
-
-//Get the current user user --->
+//Get the current user --->
 router.get('/currentuser', (req, res) => {
-  console.log(session);
-  var sql = "SELECT * FROM accounts WHERE username ='" + session.userid + "'";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    res.json(result);})
+    if ((typeof session) != 'undefined') {
+        let sql = "SELECT * FROM accounts WHERE username ='" + session.userid + "'";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        })
+    } else {
+        console.log('No user logged in.')
+    }
 });
-
 module.exports = router;
