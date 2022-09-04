4fb46018ec9d0a4722fd667a0cd2d42d1270c2ac
Thomas Durlacher <thomas.durlacher@gmail.com>
2022-09-04 13:36:06 +0200

    2nd web api included

--- server.js
+++ server.js
@@ -2,11 +2,33 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const path = require('path');
+const session = require('express-session');
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+
+
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'SECRET',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60*60*60*24,
+    httpOnly: true,
+    domain:'localhost:8000',
+    path: '/',
+    secure: false
+  }
+}));
+
+
 app.use("/api/users", require("./routes/api/users"));
 app.use(express.static(path.join(__dirname, 'frontend')));
 app.listen(port, (error) => {
@@ -16,3 +38,6 @@ app.listen(port, (error) => {
         console.log(`Server listening at http://localhost:${port}`)
     }
 });
+
+
+module.exports = session;
